0a7358730f00dc6030fef5d56f4bcf6f66fbc7eb
Michel Aquino <michel.has@gmail.com>
2017-03-08 23:33:22 -0300

    Move do folder producer

--- producer/main.go
+++ producer/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/michelaquino/rabbit_mq_test/context"
+	"github.com/michelaquino/rabbit_mq_test/producer/context"
 )
 
 func main() {
